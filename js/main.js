89f540cb6737cf454e288573ca364c9747c14592
abdoatef <abdoatef861@gmail.com>
2024-06-11 01:44:48 +0300

    Ui Changes

--- js/main.js
+++ js/main.js
@@ -1,52 +1,183 @@
 /*------------------NavBar ----------------------- */
-const lists =  document.querySelectorAll('.mainSection .list button')
-const sections =  document.querySelectorAll('.mainSection section')
+const lists = document.querySelectorAll(".mainSection .list button");
+const sections = document.querySelectorAll(".mainSection section");
 
 for (let i = 0; i < lists.length; i++) {
-  lists[i].addEventListener('click', ()=>{
+  lists[i].addEventListener("click", () => {
     for (let i = 0; i < lists.length; i++) {
-      lists[i].classList.remove('active');
-      sections[i].classList.add('d-none');
+      lists[i].classList.remove("active");
+      sections[i].classList.add("d-none");
     }
-    lists[i].classList.add('active');
-    sections[i].classList.remove('d-none');
+    lists[i].classList.add("active");
+    sections[i].classList.remove("d-none");
     window.scrollTo(0, 0);
-  })
+  });
 }
 
+const aside = document.querySelector("aside");
+const contactsToggle = document.querySelector("aside .showContactsToggle");
+const contactDetails = document.querySelector("aside .contactDetails");
+const showContactsToggleIcon = document.querySelector(
+  "aside .showContactsToggleIcon"
+);
 
-const aside = document.querySelector('aside')
-const contactsToggle = document.querySelector('aside .showContactsToggle')
-const contactDetails = document.querySelector('aside .contactDetails')
-const showContactsToggleIcon = document.querySelector('aside .showContactsToggleIcon')
+contactsToggle.addEventListener("click", () => {
+  aside.classList.toggle("active");
+  contactDetails.classList.toggle("opacity-100");
+  showContactsToggleIcon.classList.toggle("fa-angle-up");
+});
 
-contactsToggle.addEventListener('click', ()=>{
-  aside.classList.toggle('active')
-  contactDetails.classList.toggle('opacity-100')
-  showContactsToggleIcon.classList.toggle('fa-angle-up')
-})
+const projectsCo = document.getElementById("projectsCo");
+const ProjectsData = [
+  {
+    name: "Exclusive E-commerce",
+    lang: "React js",
+    imgSrc: "../assets/exclusive.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Exclusive",
+    hostSrc: "https://exclusive-store1.vercel.app",
+  },
+  {
+    name: "Pharmacy management system",
+    lang: "React js",
+    imgSrc: "../assets/pharmacy.webp",
+    codeSrc:
+      "https://github.com/Abdo-Atef/Pharmacy-inventory-management-system",
+    hostSrc: "https://www.youtube.com/watch?v=T1TxiPJPPQg",
+  },
+  {
+    name: "Induz",
+    lang: "React js, Tailwind",
+    imgSrc: "../assets/induz.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Induz",
+    hostSrc: "https://induz-ten.vercel.app/",
+  },
+  {
+    name: "Notion",
+    lang: "React Native",
+    imgSrc: "../assets/notion.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Notion",
+    hostSrc: "",
+  },
+  {
+    name: "To-Do-List App",
+    lang: "HTML, CSS, JS",
+    imgSrc: "../assets/todo.webp",
+    codeSrc: "https://github.com/Abdo-Atef/To-Do-List",
+    hostSrc: "https://abdo-tech.me/To-Do-List/",
+  },
+  {
+    name: "NFT_MarketPlace",
+    lang: "React Native",
+    imgSrc: "../assets/nft2.webp",
+    codeSrc: "https://github.com/Abdo-Atef/NFT_MarketPlace",
+    hostSrc: "",
+  },
+  {
+    name: "AbdoFolio",
+    lang: "HTML, CSS",
+    imgSrc: "../assets/Abdofolio.webp",
+    codeSrc: "https://github.com/Abdo-Atef/AbdoFolio",
+    hostSrc: "https://abdo-atef.github.io/AbdoFolio/",
+  },
+  {
+    name: "Simple Dashboard",
+    lang: "HTML, CSS",
+    imgSrc: "../assets/Dashboard.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Dashboard",
+    hostSrc: "https://abdo-tech.me/Dashboard/",
+  },
+  {
+    name: "Weather App",
+    lang: "HTML, CSS, JS",
+    imgSrc: "../assets/weather.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Weather-Project",
+    hostSrc: "https://abdo-atef.github.io/Weather-Project/",
+  },
+  {
+    name: "Games Website",
+    lang: "HTML, CSS, JS",
+    imgSrc: "../assets/game.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Game-oop",
+    hostSrc: "https://abdo-tech.me/Game-oop/",
+  },
+  {
+    name: "Danials",
+    lang: "HTML, CSS, Bootstrap",
+    imgSrc: "../assets/danials.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Danials",
+    hostSrc: "https://abdo-tech.me/Danials/",
+  },
 
+  {
+    name: "Quiz App",
+    lang: "HTML, CSS, JS",
+    imgSrc: "../assets/quiz.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Quiz-App",
+    hostSrc: "https://abdo-tech.me/Quiz-App/",
+  },
 
+  {
+    name: "Bookmark App",
+    lang: "HTML, CSS, JS",
+    imgSrc: "../assets/Bookmark.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Bookmark",
+    hostSrc: "https://abdo-tech.me/Bookmark/",
+  },
 
+  {
+    name: "DevFolio",
+    lang: "HTML, CSS, Bootstrap",
+    imgSrc: "../assets/DevFolio.webp",
+    codeSrc: "https://github.com/Abdo-Atef/DevFolio",
+    hostSrc: "https://abdo-tech.me/DevFolio/",
+  },
+  {
+    name: "Kasper",
+    lang: "HTML, CSS",
+    imgSrc: "../assets/kasper.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Kasper",
+    hostSrc: "https://abdo-tech.me/Kasper/",
+  },
+  {
+    name: "Leon",
+    lang: "HTML, CSS",
+    imgSrc: "../assets/Leon.webp",
+    codeSrc: "https://github.com/Abdo-Atef/Leon",
+    hostSrc: "https://abdo-tech.me/Leon/",
+  },
+];
 
+projectsCo.innerHTML = `
 
+  ${ProjectsData.map(
+    (project) => `
+      <div>
+        <figure>
+          <img src="${
+            project.imgSrc
+          }" class="w-100 rounded-3" lazy="loading" alt="${project.name}">
+          <div class="resources_links">
+            ${
+              project.hostSrc
+                ? `
+            <a href="${project.hostSrc}" target="_blank">
+              <i class="bi bi-eye"></i>
+            </a>
+            `
+                : ""
+            }
+            <a href="${project.codeSrc}" target="_blank">
+              <i class="bi bi-code-slash"></i>
+            </a>
+          </div>
+        </figure>
+        <h6>${project.name}</h6>
+        <p>${project.lang}</p>
+      </div>
+    `
+  ).join("")}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+`;
 
 
 
